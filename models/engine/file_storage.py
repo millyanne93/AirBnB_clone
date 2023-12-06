ceb7f2a655e925a2ee73b5dd00230732daf9ab11
millyanne93 <nmillyanne20@gmail.com>
2023-12-06 11:01:33 -0800

    Changes to file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,23 +4,8 @@ This module contains the FileStorage class.
 """
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
-class_dict = {
-    "BaseModel": BaseModel,
-    "User": User,
-    "Place": Place,
-    "Amenity": Amenity,
-    "City": City,
-    "Review": Review,
-    "State": State
-}
+import os
+import datetime
 
 
 class FileStorage:
@@ -38,6 +23,26 @@ class FileStorage:
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
+    def class_dict(self):
+        """Returns a dictionary of classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        class_dict = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return class_dict
+
+
     def save(self):
         """Serializes objects to JSON storage file"""
         new_dict = {}
@@ -53,7 +58,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
 
-                model_classes = class_dict
+                model_classes = self.class_dict
 
                 for key, val in data.items():
                     class_name, obj_id = key.split('.')
@@ -61,3 +66,4 @@ class FileStorage:
                         self.__objects[key] = model_classes[class_name](**val)
         except FileNotFoundError:
             pass
+
