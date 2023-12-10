008aeffa722a6ac0576fdbdeeb27e2079f564cd5
millyanne93 <nmillyanne20@gmail.com>
2023-12-10 11:33:36 -0800

    modified

--- models/user.py
+++ models/user.py
@@ -7,10 +7,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """The User Model"""
+    """User class"""
 
-    # Attributes
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+    def __init__(self, *args, **kwargs):
+        """Initialize User instance"""
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")
